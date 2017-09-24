f0fcd556c69cd56ad1047549237a230db5fecbe4
advancedYJ <advanced@sjtu.edu.cn>
2017-09-24 12:49:09 +0800

    cmake_build_debug

---
+++ cmake-build-debug/CMakeFiles/vectorSort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vectorSort.dir/main.cpp.obj"
+  "vectorSort.pdb"
+  "vectorSort.exe"
+  "vectorSort.exe.manifest"
+  "libvectorSort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vectorSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
